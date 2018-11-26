e34722caad6b1a7c8e566601eb2cd98fd8ea92cb
TheBerzer <berzi.w@gmail.com>
2018-11-26 16:37:00 +0100

    Added more url matches
    Sometimes Tamper Monkey does not activate, added url matches for these cases.

--- ctr_dropdowns.user.js
+++ ctr_dropdowns.user.js
@@ -1,25 +1,26 @@
 // ==UserScript==
-// @name         CTR Userscript
+// @name         CTR style
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  fix dropdowns in CTR-> My Reports
+// @description  fix table styles in CTR-> Week
 // @author       Erland
 // @match        https://apps.ne.capgemini.com/Applications/CTR/
+// @match        https://apps.ne.capgemini.com/Applications/ctr/
 // @match        https://apps.ne.capgemini.com/applications/CTR/
+// @match        https://apps.ne.capgemini.com/applications/ctr/
+// @match        https://apps-x.nordic.capgemini.com/Applications/CTR/
+// @match        https://apps-x.nordic.capgemini.com/Applications/ctr/
 // @match        https://apps-x.nordic.capgemini.com/applications/CTR/
+// @match        https://apps-x.nordic.capgemini.com/applications/ctr/
 // @match        https://apps.ne.capgemini.com/Applications//CTR/
+// @match        https://apps.ne.capgemini.com/Applications//ctr/
 // @grant        none
-
 // ==/UserScript==
-window.onload = function() {
-    var frames = document.getElementsByName("FrameMain");
-    frames.forEach(function(myFrame){
-        var myFrameContent = (myFrame.contentWindow || myFrame.contentDocument);
-        var buttons = myFrameContent.document.body.querySelectorAll("button.fieldtxt3");
-        buttons.forEach(function(element){
-            element.addEventListener("click", function(event){
-                event.preventDefault();
-            });
-        });
+var frames = document.getElementsByName("FrameMain");
+frames.forEach(function(myFrame){
+    var myFrameContent = (myFrame.contentWindow || myFrame.contentDocument);
+    myFrame.addEventListener("load", function(event){
+        myFrameContent.document.head.innerHTML = myFrameContent.document.head.innerHTML + '<style>tr[style*="display: block;"]{display:table-row !important}</style>';
     });
-}
+});
+
